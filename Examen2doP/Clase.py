1b66109f2bf27c68e301e4431da75b59b298c0fc
121038217 <121038217@upq.edu.mx>
2023-03-15 15:33:05 -0600

    commit 15:30

--- Examen2doP/Clase.py
+++ Examen2doP/Clase.py
@@ -2,25 +2,40 @@ import string
 import random
 from tkinter import messagebox
 class GenerarMatricula:
-    def __init__(self, nombre, apellidoP, apellidoM, fechanacimiento, carrera):
+    def __init__(self, nombre, apellidoP, apellidoM, fechanacimiento, carrera,contr):
         self.nombre = nombre
-        self.apellidoP = apellidoM
+        self.apellidoP = apellidoP
         self.apellidoM = apellidoM
         self.fechanacimiento = fechanacimiento
         self.carrera = carrera
+        self.contr = contr
 
     def generar_matricula(self):
-        año = int(self.fechanacimiento.get())
-        letra = self.nombre.get()
+        añoo = int(self.fechanacimiento.get())
+        letranombre = self.nombre.get()
+        letraapellidoM = self.apellidoM.get()
+        letraapellidoP = self.apellidoP.get()
+        carreraa = self.carrera.get()
         
         caracteres = string.ascii_lowercase
-        if letra:
+        if letranombre:
+            caracteres += string.ascii_uppercase
+        if letraapellidoM:
+            caracteres += string.ascii_uppercase
+        if letraapellidoP:
+            caracteres += string.ascii_uppercase
+        if carreraa:
             caracteres += string.ascii_uppercase
         
-        letraa = ''.join(random.choice(caracteres) for i in range(letra))
-        self.contr.set(letraa)
-        añoo = ''.join(random.choice(año) for i in range(letra))
-        self.contr.set(año)
-        messagebox.showinfo("Contraseña","La contraseña generada es: " + añoo+letraa)
+        pass1 = ''.join(random.choice(caracteres) for i in range(letranombre))
+        self.contr.set(pass1)
+        pass2 = ''.join(random.choice(caracteres) for i in range(letraapellidoP))
+        self.contr.set(pass2)
+        pass3 = ''.join(random.choice(caracteres) for i in range(letraapellidoM))
+        self.contr.set(pass3)
+        pass4 = ''.join(random.choice(caracteres) for i in range(añoo))
+        self.contr.set(pass4)
+        
+        messagebox.showinfo("Matricula","La matricula generada es: " + pass1+pass2+pass3+pass4)
         
         
